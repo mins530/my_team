18b82f6ec50be79b741b34cb39d615666cf20a65
unknown <pigking41@gmail.com>
2024-06-27 20:49:01 +0900

    블로그 수정2

--- templates/javascri.js
+++ templates/javascri.js
@@ -18,10 +18,10 @@ function addComment() {
 
 // 메인페이지 Search 입력 스크립트
 const teamMembers = {
-    "정성원님" : "file:///C:/Users/%EA%B9%80%EA%B2%BD%EB%AF%BC/Desktop/%ED%8C%80%20%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/templates/team-members/member1.html",
-    "배민석님" : "file:///C:/Users/%EA%B9%80%EA%B2%BD%EB%AF%BC/Desktop/%ED%8C%80%20%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/templates/team-members/member2.html",
-    "서재일님" : "file:///C:/Users/%EA%B9%80%EA%B2%BD%EB%AF%BC/Desktop/%ED%8C%80%20%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/templates/team-members/member3.html",
-    "김경민B님" : "file:///C:/Users/%EA%B9%80%EA%B2%BD%EB%AF%BC/Desktop/%ED%8C%80%20%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/templates/team-members/member4.html",
+    "정성원님" : "team-members/member1.html",
+    "배민석님" : "team-members/member2.html",
+    "서재일님" : "team-members/member3.html",
+    "김경민B님" : "team-members/member4.html",
     
 };
 
